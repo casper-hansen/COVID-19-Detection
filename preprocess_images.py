96e2d2b7c685439148f4251aacdba9e001d6d99d
Casper Bøgeskov Hansen <casperbh.96@gmail.com>
2020-03-29 14:54:32 +0200

    Column removal works (ish)

--- preprocess_images.py
+++ preprocess_images.py
@@ -17,9 +17,12 @@ for img in images_names:
 images = np.array(images)
 
 image_rows_to_remove = {}
+image_cols_to_remove = {}
 
 for i, img in enumerate(images):
     rows_to_remove = []
+    cols_to_remove = []
+
     for j, row in enumerate(img):
         num_of_black_pixels = 0
         for k, pixel in enumerate(row):
@@ -28,15 +31,32 @@ for i, img in enumerate(images):
                 num_of_black_pixels += 1
             #print(f'{k+1} {pixel} {avg} {num_of_black_pixels}')
         
-        if num_of_black_pixels > len(row)/2:    
+        if num_of_black_pixels > len(row)/2:
             rows_to_remove.append(j)
 
+    for transposed_img in img.T:
+        for j, col in enumerate(transposed_img):
+            num_of_black_pixels = 0
+            for k, pixel in enumerate(col):
+                avg = np.average(pixel)
+                if avg < 40:
+                    num_of_black_pixels += 1
+                #print(f'{k+1} {pixel} {avg} {num_of_black_pixels}')
+            
+            if num_of_black_pixels > len(col)/2:
+                cols_to_remove.append(j)
+
     image_rows_to_remove[i] = rows_to_remove
+    image_cols_to_remove[i] = cols_to_remove
 
 new_images = []
 
+print(image_cols_to_remove)
+
 for i, img in enumerate(images):
     img = np.delete(img, image_rows_to_remove[i], axis=0)
+    img = np.delete(img, image_cols_to_remove[i], axis=1)
+    print(img.shape)
     new_images.append(img)
 
 plt.imshow(new_images[0])
